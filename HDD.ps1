b880d50993fa2d846105835160372a8f1cd9defc
Skorix <SkorixOleg@gmail.com>
2022-01-01 06:18:15 +0300

    1.0.0

--- HDD.ps1
+++ HDD.ps1
@@ -1,5 +1,5 @@
+winget install --id=EpicGames.EpicGamesLauncher  -e -i -l D:\Program Files
 winget install --id=Blizzard.BattleNet -e -i -l D:\Program Files; 
 winget install --id=CrystalDewWorld.CrystalDiskMark -e -i -l D:\Program Files;  
 winget install --id=Opera.OperaGX -e -i -l D:\Program Files; 
 winget install --id=VivaldiTechnologies.Vivaldi -e -i -l D:\Program Files;
-winget install --id=EpicGames.EpicGamesLauncher  -e -i -l D:\Program Files
