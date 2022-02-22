23b873f69aecf8bdecdeb311cb976d2f5fda1a7d
Skorix <54755559+Skorix@users.noreply.github.com>
2022-02-22 10:37:40 +0300

    0.9.0

---
+++ Other.ps1
@@ -0,0 +1,5 @@
+winget install --id=AntibodySoftware.WizTree  -e -h;
+winget install --id=CrystalDewWorld.CrystalDiskInfo  -e -h;
+winget install --id=Google.Chrome  -e -h;
+winget install --id Git.Git -e --source winget -h;
+winget install --id=Microsoft.VisualStudioCode  -e -h
