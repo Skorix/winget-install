cc614d72ebc59ce4c16c86607dd0198955fc32d0
Skorix <54755559+Skorix@users.noreply.github.com>
2022-02-25 16:01:48 +0300

    1.0.6

--- Other.ps1
+++ Other.ps1
@@ -3,5 +3,5 @@ winget install --id=Git.Git -e -h;
 winget install --id=Microsoft.VisualStudioCode  -e -h;
 winget install --id=AntibodySoftware.WizTree  -e -h;
 winget install --id=CrystalDewWorld.CrystalDiskInfo  -e -h;
-
+winget install --id=QL-Win.QuickLook  -e -h;
 winget install --id=Google.Chrome  -e -h;
