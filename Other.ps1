c8f82338210ce4b32ad827735ede2b55627680de
Skorix <54755559+Skorix@users.noreply.github.com>
2022-02-25 15:59:13 +0300

    1.0.5

--- Other.ps1
+++ Other.ps1
@@ -1,4 +1,7 @@
+winget install --id=SublimeHQ.SublimeText.4  -e -h;
+winget install --id=Git.Git -e -h;
+winget install --id=Microsoft.VisualStudioCode  -e -h;
 winget install --id=AntibodySoftware.WizTree  -e -h;
 winget install --id=CrystalDewWorld.CrystalDiskInfo  -e -h;
-winget install --id Git.Git -e --source winget -h;
-winget install --id=Microsoft.VisualStudioCode  -e -h;
+
+winget install --id=Google.Chrome  -e -h;
