201baa3dfb8573f1dae5694694a9bce4abf07e34
Skorix <54755559+Skorix@users.noreply.github.com>
2024-08-10 16:55:35 +0300

    1.0.10

---
+++ download.ps1
@@ -0,0 +1,44 @@
+winget download --id=Google.Chrome -e;
+winget download --id=SublimeHQ.SublimeText.4  -e;
+winget download --id=Telegram.TelegramDesktop -e;
+winget download --id=Git.Git  -e;
+winget download --id=Microsoft.PowerToys  -e;
+winget download --id=AntibodySoftware.WizTree -e; 
+winget download --id=CrystalDewWorld.CrystalDiskInfo  -e;
+winget download --id=Microsoft.VisualStudioCode  -e;
+
+# winget download --id=Valve.Steam -e;
+# winget download --id=GOG.Galaxy -e;
+# winget download --id=Discord.Discord -e;
+# winget download --id=Giisler.TotalCommander -e; 
+# winget download --id=RARLab.WinRAR -e;
+# winget download --id=Ubisoft.Connect -e; 
+# winget download --id=EpicGames.EpicGamesLauncher  -e;
+# winget download --id=CrystalDewWorld.CrystalDiskMark -e;  
+# winget download --id=Opera.OperaGX -e; 
+# winget download --id=VivaldiTechnologies.Vivaldi -e;
+# winget download --id=OBSProject.OBSStudio  -e;
+# winget download --id=Nvidia.RTXVoice  -e;
+# winget download --id=Mega.MEGASync -e;
+# winget download --id=Spotify.Spotify -e;
+# winget download --id=Blizzard.BattleNet -e;
+# winget download --id=QL-Win.QuickLook  -e;
+# winget download --id=Henry++.MemReduct  -e;
+# winget download --id=Python.Python.3.12  -e;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
